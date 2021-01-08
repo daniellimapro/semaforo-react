98da3a539cfd9ba16da3557b0dc2a26f2356fd99
Daniel Lima <danieldeveloper1@gmail.com>
2021-01-08 09:43:43 -0300

    função de clique e alteração do semáforo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import { 
   Container,
   BoxSemaforo,
@@ -5,12 +7,18 @@ import {
 } from './AppStyle';
 
 function App() {
+  const [semaforo, setSemaforo] = useState('pare');
+
+  const handleClick = (handle) => {
+    setSemaforo(handle);
+  }
+
   return (
     <Container>
       <BoxSemaforo>
-        <Circle active={true} color='#75C46B' />
-        <Circle active={false} color='#FECA54' />
-        <Circle active={false} color='#E74C3C' />
+        <Circle onClick={() => handleClick('siga')} active={semaforo === 'siga' ? true : false} color='#75C46B' />
+        <Circle onClick={() => handleClick('aguarde')} active={semaforo === 'aguarde' ? true : false} color='#FECA54' />
+        <Circle onClick={() => handleClick('pare')} active={semaforo === 'pare' ? true : false} color='#E74C3C' />
       </BoxSemaforo>
     </Container>
   );
